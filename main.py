71317294b6333acbddd9e3957ad9ce266fb32310
I NYM Yoga Saputra <48833887+nyomanyoga@users.noreply.github.com>
2021-06-03 21:43:09 +0700

    Social Distancing

--- main.py
+++ main.py
@@ -22,21 +22,6 @@ def create_json(r, yel, g, link_img):
 
     return image_json
 
-def mouse_click(event, x, y, flags, param):
-    global click
-    if event == cv2.EVENT_LBUTTONDOWN:
-        if len(click) < 4:
-            cv2.circle(img, (x, y), 5, (0, 0, 255), 10)
-        else:
-            cv2.circle(img, (x, y), 5, (255, 0, 0), 10)        
-        if len(click) >= 1 and len(click) <= 3:
-            cv2.line(img, (x, y), (click[len(click)-1][0], click[len(click)-1][1]), (70, 70, 70), 2)
-            if len(click) == 3:
-                cv2.line(img, (x, y), (click[0][0], click[0][1]), (70, 70, 70), 2)    
-        if "click" not in globals():
-            click = []
-        click.append((x, y))
-
 def calc_dis(vid_path, net, output_dir, ln1):
     r = []
     yel =[]
@@ -49,7 +34,8 @@ def calc_dis(vid_path, net, output_dir, ln1):
     width = int(vs.get(cv2.CAP_PROP_FRAME_WIDTH))
     fps = int(vs.get(cv2.CAP_PROP_FPS))
 
-    points = []
+    points = [[385, 402], [632, 226], [376, 117], [119, 236], [236, 239], [287, 212], [266, 262], [291, 251]]
+    # points = []
     global img
     while True:
         (grabbed, frame) = vs.read()
@@ -59,17 +45,7 @@ def calc_dis(vid_path, net, output_dir, ln1):
             # print('Done')
             break
         (H, W) = frame.shape[:2]
-        
-        # first frame will be used to draw ROI and horizontal and vertical 180 cm distance(unit length in both directions)
-        if count == 0:
-            while True:
-                img = frame
-                cv2.imshow("img", img)
-                cv2.waitKey(1)
-                if len(click) == 8:
-                    cv2.destroyWindow("img")
-                    break
-            points = click      
+
         src = np.float32(np.array(points[:4]))
         dst = np.float32([[0, H], [W, H], [W, 0], [0, 0]])
         prespective_transform = cv2.getPerspectiveTransform(src, dst)
@@ -79,8 +55,6 @@ def calc_dis(vid_path, net, output_dir, ln1):
         warped_pt = cv2.perspectiveTransform(pts, prespective_transform)[0]        
         distance_w = np.sqrt((warped_pt[0][0] - warped_pt[1][0]) ** 2 + (warped_pt[0][1] - warped_pt[1][1]) ** 2)
         distance_h = np.sqrt((warped_pt[0][0] - warped_pt[2][0]) ** 2 + (warped_pt[0][1] - warped_pt[2][1]) ** 2)
-        pnts = np.array(points[:4], np.int32)
-        cv2.polylines(frame, [pnts], True, (70, 70, 70), thickness=2)
 
         # models
         blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB=True, crop=False)
@@ -132,21 +106,11 @@ def calc_dis(vid_path, net, output_dir, ln1):
         count = count + 1
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break;
-    vs.release()
-    cv2.destroyAllWindows() 
 
 def main():
-    # Make and remove directory output
-    Path = './output' 
-    if not os.path.exists(Path):
-        os.makedirs(Path)
-    else :
-        shutil.rmtree(Path)
-        os.makedirs(Path)
-
     # Receives arguements specified by user
     parser = argparse.ArgumentParser()
-    parser.add_argument('-v', '--video_path', action='store', dest='video_path', default='./data/ex.mp4' ,
+    parser.add_argument('-v', '--video_path', action='store', dest='video_path', default='./data/ped.mp4' ,
                     help='Path for input video')
     parser.add_argument('-o', '--output_dir', action='store', dest='output_dir', default='./output/' ,
                     help='Path for Output imgs')
@@ -168,14 +132,8 @@ def main():
     net_yl = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
     ln = net_yl.getLayerNames()
     ln1 = [ln[i[0] - 1] for i in net_yl.getUnconnectedOutLayers()]
-
-    # set mouse callback 
-    cv2.namedWindow("img")
-    cv2.setMouseCallback("img", mouse_click)
-    np.random.seed(42)
     
     return calc_dis(values.video_path, net_yl, output_dir, ln1)
-
 
 
 # Flask
